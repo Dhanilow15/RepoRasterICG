87ffc62fa7cf08944043b8fd33c8db3a91d95f72
Dhanilow15 <dhanilow14@gmail.com>
2022-09-19 11:16:22 -0300

    Rectangle

---
+++ framework/C++/dia 21set/mygl.h
@@ -0,0 +1,80 @@
+#ifndef _MYGL_H_
+#define _MYGL_H_
+
+#define _R 255
+#define _G 255
+#define _B 255
+#define _A 255
+
+#include "definitions.h"
+#include <math.h>
+using namespace std;
+
+//-----------------------------------------------------------------------------
+void MyGlDraw(void);
+
+//*****************************************************************************
+int find_pixel(int pos_x, int pos_y){
+    return pos_x*4 + pos_y*IMAGE_WIDTH*4;
+}
+
+void put_pixel(int pos_x, int pos_y){
+    int MemPos = find_pixel(pos_x, pos_y);
+    FBptr[MemPos] = _R;
+    FBptr[MemPos + 1] = _G;
+    FBptr[MemPos + 2] = _B;
+    FBptr[MemPos + 3] = _A;
+}
+ 
+// function for line generation
+void bresenham1(int x1, int y1, int x2, int y2)
+{
+    int deltaY = y2 - y1;
+    int deltaX = x2 - x1;
+
+    if(x1 > x2 || y1 > y2){
+        bresenham1(x2, y2, x1, y1);
+        return;
+    }
+
+    if(deltaX == 0){
+        for (int i = y1; i <= y2; i++){
+            put_pixel(x1, i);
+        }
+    }
+    else if(deltaY == 0){
+        for (int i = x1; i <= x2; i++){
+            put_pixel(i, y1);
+        }
+    }
+    else{
+        float coeficienteAngular = deltaY/deltaX;
+        float coeficienteLinear = y1 - coeficienteAngular*float(x1);
+
+        for (int i = x1; i <= x2; i++){
+            int j = round(coeficienteAngular*float(i) + coeficienteLinear);
+            //printf("%d e %d", i, j);
+            put_pixel(i, j);
+        }
+    }
+}
+
+void triangle(int *ponto1, int *ponto2, int *ponto3){
+    //cada ponto tera um x,y e se ligara ao outro
+
+    bresenham1(ponto1[0], ponto1[1], ponto2[0], ponto2[1]);
+    bresenham1(ponto2[0], ponto2[1], ponto3[0], ponto3[1]);
+    bresenham1(ponto3[0], ponto3[1], ponto1[0], ponto1[1]);
+}
+//*****************************************************************************
+
+void rectangle(int *origem, int comprimento, int altura){
+    //a partir da origem desenha o triangulo
+
+    bresenham1(origem[0], origem[1], origem[0]+comprimento, origem[1]);
+    bresenham1(origem[0]+comprimento, origem[1], origem[0]+comprimento, origem[1]+altura);
+    bresenham1(origem[0]+comprimento, origem[1]+altura, origem[0], origem[1]+altura);
+    bresenham1(origem[0], origem[1]+altura, origem[0], origem[1]);
+}
+
+#endif // _MYGL_H_
